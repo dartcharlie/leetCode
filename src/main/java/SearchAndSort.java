072a8479fb8cfdeeae1bdd0c4129d28c3acc9baa
Zhifei Song <zsong@linkedin.com>
2016-09-15 16:45:49 -0700

    apply formatting

--- src/main/java/SearchAndSort.java
+++ src/main/java/SearchAndSort.java
@@ -55,40 +55,40 @@ public class SearchAndSort {
 
   public List<Integer> findSubstring(String s, String[] words) {
     List<Integer> indices = new ArrayList<>();
-    if(s == null || s.length() == 0 || words == null || words.length == 0){
+    if (s == null || s.length() == 0 || words == null || words.length == 0) {
 
-    }else{
+    } else {
       int wordLen = words[0].length();
       int strLen = s.length();
       int wordNum = words.length;
       int currStart, currEnd;
-      Map<String,Integer> wordMap = new HashMap<>();
-      for(String word: words){
-        if(wordMap.containsKey(word)){
+      Map<String, Integer> wordMap = new HashMap<>();
+      for (String word : words) {
+        if (wordMap.containsKey(word)) {
           int currCount = wordMap.get(word);
-          wordMap.put(word,currCount+1);
-        }else{
-          wordMap.put(word,1);
+          wordMap.put(word, currCount + 1);
+        } else {
+          wordMap.put(word, 1);
         }
       }
-      for(currStart = 0;currStart <= strLen - (wordNum*wordLen); currStart++){
-        Map<String,Integer> currMap = new HashMap<String,Integer>(wordMap);
+      for (currStart = 0; currStart <= strLen - (wordNum * wordLen); currStart++) {
+        Map<String, Integer> currMap = new HashMap<String, Integer>(wordMap);
         int matchCounter = 0;
-        for(currEnd = currStart + wordLen; currEnd <= strLen; currEnd = currEnd + wordLen){
-          String sub = s.substring(currEnd - wordLen,currEnd);
-          if(currMap.containsKey(sub)){
+        for (currEnd = currStart + wordLen; currEnd <= strLen; currEnd = currEnd + wordLen) {
+          String sub = s.substring(currEnd - wordLen, currEnd);
+          if (currMap.containsKey(sub)) {
             int remainingCount = currMap.get(sub);
-            if(remainingCount > 0) {
-              currMap.put(sub,remainingCount-1);
+            if (remainingCount > 0) {
+              currMap.put(sub, remainingCount - 1);
               matchCounter++;
-            }else{
+            } else {
               break;
             }
-          }else{
+          } else {
             break;
           }
         }
-        if(matchCounter == wordNum){
+        if (matchCounter == wordNum) {
           indices.add(currStart);
         }
       }
@@ -98,7 +98,7 @@ public class SearchAndSort {
 
   /**
    * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-   *
+   * <p>
    * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
    * for example
    * 1, 2, 3 -> 1, 3, 2
@@ -106,77 +106,77 @@ public class SearchAndSort {
    * 5,4,3,2,1 -> 1,2,3,4,5
    */
   public void nextPermutation(int[] nums) {
-    if(nums != null && nums.length > 0){
+    if (nums != null && nums.length > 0) {
       int numsLen = nums.length;
       int cutOffIndex = -1;
-      for(int i=numsLen-1;i>0;i--){
-        if(nums[i] > nums[i-1]){
-          cutOffIndex = i-1;
+      for (int i = numsLen - 1; i > 0; i--) {
+        if (nums[i] > nums[i - 1]) {
+          cutOffIndex = i - 1;
           break;
         }
       }
-      if(cutOffIndex > -1){
+      if (cutOffIndex > -1) {
         int swapPos = cutOffIndex;
-        for(int i=numsLen-1;i>cutOffIndex;i--){
-          if(nums[i] > nums[cutOffIndex]){
+        for (int i = numsLen - 1; i > cutOffIndex; i--) {
+          if (nums[i] > nums[cutOffIndex]) {
             swapPos = i;
             break;
           }
         }
-        swap(nums,cutOffIndex,swapPos);
+        swap(nums, cutOffIndex, swapPos);
       }
-      reverse(nums,cutOffIndex+1);
+      reverse(nums, cutOffIndex + 1);
     }
   }
 
-  private void swap(int[] nums, int i, int j){
+  private void swap(int[] nums, int i, int j) {
     int temp = nums[i];
     nums[i] = nums[j];
     nums[j] = temp;
   }
 
-  private void reverse(int[] nums, int i){
+  private void reverse(int[] nums, int i) {
     int numsLen = nums.length;
     int start = i;
-    int end = numsLen -1;
-    while(start < end){
-      swap(nums,start,end);
+    int end = numsLen - 1;
+    while (start < end) {
+      swap(nums, start, end);
       start++;
       end--;
     }
   }
 
-  public int binarySearchLeft(int[] nums,  int target){
+  public int binarySearchLeft(int[] nums, int target) {
     int low = 0;
-    int high = nums.length -1;
+    int high = nums.length - 1;
     int candidate = -1;
-    while(low <= high){
-      int mid = (high - low )/2 + low;
-      if(nums[mid] < target){
-        low = mid +1;
-      }else if(nums[mid] > target){
-        high = mid-1;
-      }else{
+    while (low <= high) {
+      int mid = (high - low) / 2 + low;
+      if (nums[mid] < target) {
+        low = mid + 1;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
         candidate = mid;
-        high = mid-1;
+        high = mid - 1;
       }
     }
     return candidate;
   }
 
-  public int binarySearchRight(int[] nums,  int target){
+  public int binarySearchRight(int[] nums, int target) {
     int low = 0;
-    int high = nums.length -1;
+    int high = nums.length - 1;
     int candidate = -1;
-    while(low <= high){
-      int mid = (high - low)/2 + low;
-      if(nums[mid] < target){
-        low = mid +1;
-      }else if(nums[mid] > target){
-        high = mid-1;
-      }else{
+    while (low <= high) {
+      int mid = (high - low) / 2 + low;
+      if (nums[mid] < target) {
+        low = mid + 1;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
         candidate = mid;
-        low = mid+1;
+        low = mid + 1;
       }
     }
     return candidate;
@@ -184,7 +184,8 @@ public class SearchAndSort {
 
   /**
    * Given a sorted array of integers, find the starting and ending position of a given target value.
-   * @param nums sorted array of integers
+   *
+   * @param nums   sorted array of integers
    * @param target target value
    * @return starting and ending position for target value
    * example: Given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].
@@ -192,12 +193,12 @@ public class SearchAndSort {
    */
   public int[] searchRange(int[] nums, int target) {
     int[] range = new int[2];
-    int leftPos = binarySearchLeft(nums,target);
-    if(leftPos == -1){
+    int leftPos = binarySearchLeft(nums, target);
+    if (leftPos == -1) {
       range[0] = -1;
       range[1] = -1;
-    }else{
-      int rightPos = binarySearchRight(nums,target);
+    } else {
+      int rightPos = binarySearchRight(nums, target);
       range[0] = leftPos;
       range[1] = rightPos;
     }
@@ -208,27 +209,28 @@ public class SearchAndSort {
    * leetcode 35
    * Given a sorted array and a target value, return the index if the target is found.
    * If not, return the index where it would be if it were inserted in order.
+   *
    * @param nums
    * @param target
    * @return
    */
   public int searchInsert(int[] nums, int target) {
     int low = 0;
-    int high = nums.length-1;
+    int high = nums.length - 1;
     int ret = -1;
-    while(low <= high){
-      int mid = (high - low)/2 + low;
-      if(nums[mid] > target){
-        high = mid -1;
-      }else if(nums[mid] < target){
-        low = mid +1;
-      }else{
+    while (low <= high) {
+      int mid = (high - low) / 2 + low;
+      if (nums[mid] > target) {
+        high = mid - 1;
+      } else if (nums[mid] < target) {
+        low = mid + 1;
+      } else {
         ret = mid;
         break;
       }
     }
-    if(ret == -1){
-      ret = low > high? low: nums[low] > target? low :low+1;
+    if (ret == -1) {
+      ret = low > high ? low : nums[low] > target ? low : low + 1;
     }
     return ret;
   }
--- src/test/java/SearchAndSortTest.java
+++ src/test/java/SearchAndSortTest.java
@@ -33,83 +33,84 @@ public class SearchAndSortTest {
     assertEquals(_searchAndSort.isPerfectSquare(2147483647), false);
   }
 
-  @Test void findSubstringTest() {
+  @Test
+  void findSubstringTest() {
     String s1 = "barfoothefoobarman";
-    String[] words1 = {"foo","bar"};
+    String[] words1 = {"foo", "bar"};
     List<Integer> res1 = new ArrayList<>();
     res1.add(0);
     res1.add(9);
-    assertEquals(_searchAndSort.findSubstring(s1,words1), res1);
+    assertEquals(_searchAndSort.findSubstring(s1, words1), res1);
 
     String s2 = "wordgoodgoodgoodbestword";
-    String[] words2 = {"word","good","best","good"};
+    String[] words2 = {"word", "good", "best", "good"};
     List<Integer> res2 = new ArrayList<>();
     res2.add(8);
-    assertEquals(_searchAndSort.findSubstring(s2,words2), res2);
+    assertEquals(_searchAndSort.findSubstring(s2, words2), res2);
   }
 
   @Test
-  void nextPermutationTest(){
-    int[] nums1 = new int[] {1,2,3};
-    int[] res1 = new int[] {1,3,2};
+  void nextPermutationTest() {
+    int[] nums1 = new int[]{1, 2, 3};
+    int[] res1 = new int[]{1, 3, 2};
     _searchAndSort.nextPermutation(nums1);
-    assertEquals(nums1,res1);
+    assertEquals(nums1, res1);
 
-    int[] nums2 = new int[] {4,3,2,1};
-    int[] res2 = new int[] {1,2,3,4};
+    int[] nums2 = new int[]{4, 3, 2, 1};
+    int[] res2 = new int[]{1, 2, 3, 4};
     _searchAndSort.nextPermutation(nums2);
-    assertEquals(nums2,res2);
+    assertEquals(nums2, res2);
 
-    int[] nums3 = new int[] {1,3,2,7,5};
-    int[] res3 = new int[] {1,3,5,2,7};
+    int[] nums3 = new int[]{1, 3, 2, 7, 5};
+    int[] res3 = new int[]{1, 3, 5, 2, 7};
     _searchAndSort.nextPermutation(nums3);
-    assertEquals(nums3,res3);
+    assertEquals(nums3, res3);
   }
 
   @Test
   void binarySearchLeftTest() {
-    int[] nums1 = new int[] {5, 7, 7, 8, 8, 10};
-    assertEquals(_searchAndSort.binarySearchLeft(nums1,8),3);
+    int[] nums1 = new int[]{5, 7, 7, 8, 8, 10};
+    assertEquals(_searchAndSort.binarySearchLeft(nums1, 8), 3);
 
-    int[] nums2 = new int[] {8, 8, 8, 8, 8, 8};
-    assertEquals(_searchAndSort.binarySearchLeft(nums2,8),0);
+    int[] nums2 = new int[]{8, 8, 8, 8, 8, 8};
+    assertEquals(_searchAndSort.binarySearchLeft(nums2, 8), 0);
 
-    int[] nums3 = new int[] {5, 7, 7, 8, 8, 10};
-    assertEquals(_searchAndSort.binarySearchLeft(nums1,3),-1);
+    int[] nums3 = new int[]{5, 7, 7, 8, 8, 10};
+    assertEquals(_searchAndSort.binarySearchLeft(nums1, 3), -1);
   }
 
   @Test
   void binarySearchRightTest() {
-    int[] nums1 = new int[] {5, 7, 7, 8, 8, 10};
-    assertEquals(_searchAndSort.binarySearchRight(nums1,8),4);
+    int[] nums1 = new int[]{5, 7, 7, 8, 8, 10};
+    assertEquals(_searchAndSort.binarySearchRight(nums1, 8), 4);
 
-    int[] nums2 = new int[] {8, 8, 8, 8, 8, 8};
-    assertEquals(_searchAndSort.binarySearchRight(nums2,8),5);
+    int[] nums2 = new int[]{8, 8, 8, 8, 8, 8};
+    assertEquals(_searchAndSort.binarySearchRight(nums2, 8), 5);
 
-    int[] nums3 = new int[] {5, 7, 7, 8, 8, 10};
-    assertEquals(_searchAndSort.binarySearchRight(nums3,3),-1);
+    int[] nums3 = new int[]{5, 7, 7, 8, 8, 10};
+    assertEquals(_searchAndSort.binarySearchRight(nums3, 3), -1);
   }
 
   @Test
   void searchRangeTest() {
-    int[] nums1 = new int[] {5, 7, 7, 8, 8, 10};
-    int[] range1 = new int[] {3,4};
-    assertEquals(_searchAndSort.searchRange(nums1,8),range1);
+    int[] nums1 = new int[]{5, 7, 7, 8, 8, 10};
+    int[] range1 = new int[]{3, 4};
+    assertEquals(_searchAndSort.searchRange(nums1, 8), range1);
 
-    int[] nums2 = new int[] {5, 7, 7, 8, 8, 10};
-    int[] range2 = new int[] {-1,-1};
-    assertEquals(_searchAndSort.searchRange(nums1,3),range2);
+    int[] nums2 = new int[]{5, 7, 7, 8, 8, 10};
+    int[] range2 = new int[]{-1, -1};
+    assertEquals(_searchAndSort.searchRange(nums1, 3), range2);
   }
 
   @Test
   void searchInsertTest() {
-    int[] nums = new int[] {1,3,5,6};
-    int target1 = 5, target2 =2,target3=7,target4=0;
-    int res1 = 2, res2 = 1, res3= 4, res4=0;
-    assertEquals(_searchAndSort.searchInsert(nums,target1),res1);
-    assertEquals(_searchAndSort.searchInsert(nums,target2),res2);
-    assertEquals(_searchAndSort.searchInsert(nums,target3),res3);
-    assertEquals(_searchAndSort.searchInsert(nums,target4),res4);
+    int[] nums = new int[]{1, 3, 5, 6};
+    int target1 = 5, target2 = 2, target3 = 7, target4 = 0;
+    int res1 = 2, res2 = 1, res3 = 4, res4 = 0;
+    assertEquals(_searchAndSort.searchInsert(nums, target1), res1);
+    assertEquals(_searchAndSort.searchInsert(nums, target2), res2);
+    assertEquals(_searchAndSort.searchInsert(nums, target3), res3);
+    assertEquals(_searchAndSort.searchInsert(nums, target4), res4);
   }
 }
 
